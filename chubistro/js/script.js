0b08e7cb6b966df95f26837131a4bd77e62fca42
proy01 <pronoy.roy2808@gmail.com>
2021-07-27 21:57:42 +0530

    changed autohide and made mobile-menu center aligned

--- chubistro/js/script.js
+++ chubistro/js/script.js
@@ -1,11 +1,9 @@
 let navButton = document.getElementById("nav_button");
-console.log(navButton);
 
-navButton.addEventListener("blur", function (){
+navButton.addEventListener('blur', function (){
     if (window.innerWidth < 768) {
-        let doCollapse = document.getElementById("navbarSupportedContent");
-        new bootstrap.Collapse(doCollapse, {
+        new bootstrap.Collapse("#navbarSupportedContent", {
             toggle: true
-        })
+        });
     }
-});
+})
